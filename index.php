414b5cb0960c3fafac0122f6c7bd1c29618fb5ce
Ramariarison <andryramariarison@gmail.com>
2026-01-12 18:26:13 +0300

    mise en place du navbar dans la page d'accueil

--- index.php
+++ index.php
@@ -3,27 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/accueil.css">
     <title>Accueil</title>
 </head>
 <body>
-    <div class="main">
-        <div class="nav">
+    <div class="container">
+        <div class="top">
             <nav>
-                
+                <div class="logo">note<span class="second">me</span></div>
+                <ul class="navlink">
+                    <li><a href="#">Click here to show more details</a></li>
+                </ul>
+                <div class="btns">
+                    <button class="btn">Login</button>
+                    <a class="link" href="">Register ?</a>
+                </div>
             </nav>
         </div>
-        <div class="body">
+        <!--<div class="center">
             <div class="title">
-
+                <h1>Bienvenue sur notre plateforme</h1>
             </div>
             <div class="message">
-
+                <p>
+                    C'est une plateforme dédiée à la gestion de notes personnelles.
+                </p>
             </div>
-            <div class="button">
-
+            <div class="bottom">
+                <button>Login</button>
+                <button>register</button>
             </div>
-        </div>
+        </div>-->
     </div>
 </body>
 </html>
