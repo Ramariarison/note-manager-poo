f019dfe5800075b35adc42dec7e0a322877ae019
Ramariarison <andryramariarison@gmail.com>
2026-01-14 22:07:16 +0300

    feat: Mise en place de la structure de base de l'interface dédiée aux notes

---
+++ views/note-interface.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/note-interface.css">
+    <title>Page de notes</title>
+</head>
+<body>
+    <div class="layout">
+        <aside class="sidebar">
+            Sidebar
+        </aside>
+        <main class="right-content">
+            Contenu principal
+        </main>
+    </div>
+</body>
+</html>
