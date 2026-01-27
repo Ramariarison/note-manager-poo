994814c75e319a16d3f1438f493c300fea7a958e
Ramariarison <andryramariarison@gmail.com>
2026-01-27 22:17:39 +0300

    feat: Amélioration du design pour le footer du card

--- views/note-interface.php
+++ views/note-interface.php
@@ -71,12 +71,12 @@
                 </div>
                 <div class="header-actions">
                     <!-- Recherche -->
-                     <div class="search-box">
+                    <div class="search-box">
                         <i class="fa fa-search"></i>
                         <input type="text" placeholder="Search">
-                     </div>
-                     <!-- Filtre -->
-                      <div class="filter-box">
+                    </div>
+                    <!-- Filtre -->
+                    <div class="filter-box">
                         <i class="fa fa-filter"></i>
                         <select>
                             <option value="None">Sort</option>
@@ -85,22 +85,22 @@
                             <option value="Medium">Medium</option>
                             <option value="Low">Low</option>
                         </select>
-                      </div>
-                      <!-- Bouton d'ajout
-                       <div class="btn-add">
+                    </div>
+                    <!-- Bouton d'ajout
+                    <div class="btn-add">
                             <i class="fa fa-plus"></i>
                             Add note
-                       </div>
-                       -->
+                    </div>
+                    -->
                 </div>
             </div>
             <!-- Liste de notes -->
-             <div class="notes-grid">
-                 <!-- Card -->
-                 <div class="note-card">
+            <div class="notes-grid">
+                <!-- Card -->
+                <div class="note-card">
                     <div class="card-header">
                         <div class="note-priority">
-                            <span class="priority-level">H</span>
+                            <span class="priority-level">C</span>
                         </div>
                         <div class="btn-to-pin-note">
                             <button class="pin-note">pin note</button>
@@ -127,6 +127,7 @@
                         </div>
                     </div>
                     <div class="card-footer">
+                        <!--
                         <div class="actions-btns">
                             <button class="view">
                                 <a href="#">view</a>
@@ -138,13 +139,29 @@
                                 <a href="#">delete</a>
                             </button>
                         </div>
+                        -->
+                        <div class="actions-btns">
+                            <div class="left-actions">
+                                <button class="view">
+                                    <i class="fa fa-eye"></i>
+                                    <span>view</span>
+                                </button>
+                                <button class="edit">
+                                    <i class="fa fa-pencil"></i>
+                                    <span>edit</span>
+                                </button>
+                            </div>
+                            <div class="delete-icon">
+                                <i class="fa fa-trash"></i>
+                            </div>
+                        </div>
                     </div>
-                 </div>
-                                  <!-- Card -->
-                 <div class="note-card">
+                </div>
+                                <!-- Card -->
+                <div class="note-card">
                     <div class="card-header">
                         <div class="note-priority">
-                            <span class="priority-level">H</span>
+                            <span class="priority-level">C</span>
                         </div>
                         <div class="btn-to-pin-note">
                             <button class="pin-note">pin note</button>
@@ -171,6 +188,7 @@
                         </div>
                     </div>
                     <div class="card-footer">
+                        <!--
                         <div class="actions-btns">
                             <button class="view">
                                 <a href="#">view</a>
@@ -182,13 +200,29 @@
                                 <a href="#">delete</a>
                             </button>
                         </div>
+                        -->
+                        <div class="actions-btns">
+                            <div class="left-actions">
+                                <button class="view">
+                                    <i class="fa fa-eye"></i>
+                                    <span>view</span>
+                                </button>
+                                <button class="edit">
+                                    <i class="fa fa-pencil"></i>
+                                    <span>edit</span>
+                                </button>
+                            </div>
+                            <div class="delete-icon">
+                                <i class="fa fa-trash"></i>
+                            </div>
+                        </div>
                     </div>
-                 </div>
-                                  <!-- Card -->
-                 <div class="note-card">
+                </div>
+                                <!-- Card -->
+                <div class="note-card">
                     <div class="card-header">
                         <div class="note-priority">
-                            <span class="priority-level">H</span>
+                            <span class="priority-level">C</span>
                         </div>
                         <div class="btn-to-pin-note">
                             <button class="pin-note">pin note</button>
@@ -215,6 +249,7 @@
                         </div>
                     </div>
                     <div class="card-footer">
+                        <!--
                         <div class="actions-btns">
                             <button class="view">
                                 <a href="#">view</a>
@@ -226,13 +261,29 @@
                                 <a href="#">delete</a>
                             </button>
                         </div>
+                        -->
+                        <div class="actions-btns">
+                            <div class="left-actions">
+                                <button class="view">
+                                    <i class="fa fa-eye"></i>
+                                    <span>view</span>
+                                </button>
+                                <button class="edit">
+                                    <i class="fa fa-pencil"></i>
+                                    <span>edit</span>
+                                </button>
+                            </div>
+                            <div class="delete-icon">
+                                <i class="fa fa-trash"></i>
+                            </div>
+                        </div>
                     </div>
-                 </div>
-                                  <!-- Card -->
-                 <div class="note-card">
+                </div>
+                                <!-- Card -->
+                <div class="note-card">
                     <div class="card-header">
                         <div class="note-priority">
-                            <span class="priority-level">H</span>
+                            <span class="priority-level">C</span>
                         </div>
                         <div class="btn-to-pin-note">
                             <button class="pin-note">pin note</button>
@@ -259,6 +310,7 @@
                         </div>
                     </div>
                     <div class="card-footer">
+                        <!--
                         <div class="actions-btns">
                             <button class="view">
                                 <a href="#">view</a>
@@ -270,9 +322,86 @@
                                 <a href="#">delete</a>
                             </button>
                         </div>
+                        -->
+                        <div class="actions-btns">
+                            <div class="left-actions">
+                                <button class="view">
+                                    <i class="fa fa-eye"></i>
+                                    <span>view</span>
+                                </button>
+                                <button class="edit">
+                                    <i class="fa fa-pencil"></i>
+                                    <span>edit</span>
+                                </button>
+                            </div>
+                            <div class="delete-icon">
+                                <i class="fa fa-trash"></i>
+                            </div>
+                        </div>
                     </div>
-                 </div>
-             </div>
+                </div>
+                                <!-- Card -->
+                <div class="note-card">
+                    <div class="card-header">
+                        <div class="note-priority">
+                            <span class="priority-level">C</span>
+                        </div>
+                        <div class="btn-to-pin-note">
+                            <button class="pin-note">pin note</button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="note-date-and-hour">
+                            <div class="note-date">
+                                <span class="date">23-11-2020</span>
+                            </div>
+                            <div class="note-hour">
+                                <span class="hour">15h:30min</span>
+                            </div>
+                        </div>
+                        <div class="note-title">
+                            <span>Amazon</span>
+                        </div>
+                        <div class="note-content">
+                            <span class="content">
+                                L'amour est un sentiment profond d'affection et d'attachement, 
+                                unissant deux personnes par un désir de proximité physique, 
+                                intellectuelle ou émotionnelle.
+                            </span>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <!--
+                        <div class="actions-btns">
+                            <button class="view">
+                                <a href="#">view</a>
+                            </button>
+                            <button class="edit">
+                                <a href="#">edit</a>
+                            </button>
+                            <button class="delete">
+                                <a href="#">delete</a>
+                            </button>
+                        </div>
+                        -->
+                        <div class="actions-btns">
+                            <div class="left-actions">
+                                <button class="view">
+                                    <i class="fa fa-eye"></i>
+                                    <span>view</span>
+                                </button>
+                                <button class="edit">
+                                    <i class="fa fa-pencil"></i>
+                                    <span>edit</span>
+                                </button>
+                            </div>
+                            <div class="delete-icon">
+                                <i class="fa fa-trash"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
