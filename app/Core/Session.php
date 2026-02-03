2bbe894bc4d914be046b17af6c5d84348e567321
Ramariarison <andryramariarison@gmail.com>
2026-02-03 23:19:31 +0300

    fix: correct session_status function call

--- app/Core/Session.php
+++ app/Core/Session.php
@@ -5,7 +5,7 @@ namespace App\Core;
 class Session {
     public function __construct()
     {
-        if (session_status === PHP_SESSION_NONE) {
+        if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
     }
