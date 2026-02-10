01d1b1fb3c320509deeba48fe2fa0ec4f3dff833
Ramariarison <andryramariarison@gmail.com>
2026-02-10 15:56:13 +0300

    refactor: Ajustement du point d'entré en fonction du nouveau router

--- public/index.php
+++ public/index.php
@@ -4,11 +4,28 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use App\Core\Session;
 use App\Core\Router;
+use App\Services\AuthService;
+use App\Repositories\UserRepository;
+use App\Controllers\AuthController;
+use App\Controllers\HomeController;
 
+// Création des objets nécessaires
 $session = new Session();
+$userRepository = new UserRepository();
+$authService = new AuthService($userRepository, $session); 
 
+// Créer le contrôleur avec injection de dépendances si nécessaire
+$homeController = new HomeController();
+$authController = new AuthController($authService, $session);
+
+// Initialiser le routeur
 $router = new Router();
 
-$router->get('/', 'HomeController@index');
+// Routes
+$router->get('/', [$homeController, 'index']);
+$router->get('/loginPage', [$authController, 'showLogin']);
+$router->get('/registerPage', [$authController, 'showRegister']);
+$router->post('/register', [$authController, 'register']);
+$router->post('/login', [$authController, 'login']);
 
 $router->dispatch();
